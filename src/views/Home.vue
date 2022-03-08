e9a1f5261591dcc54f67d5a670d844ec7e33aeca
kimdoomin <kimduumin@withnetworks.com>
2022-03-08 15:44:52 +0900

    trello 추가/삭제/수정 fe 작업 완료

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="itemBox" v-for="element in data" :key="element.id">
+    <div class="itemBox" v-for="(element, elementIdx) in items" :key="element.id">
       <div class="itemHeader">
         <div class="title">
           {{element.name}}
@@ -11,26 +11,50 @@
       </div>
       <div class="itemBody">  
         <draggable class="dragable" group="people" v-model="element.rows" @start="drag=true" @end="drag=false">
-          <div class="item" v-for="item in element.rows" :key="item.id">{{item.name}}</div>
+          <div class="item" v-for="(item, itemIdx) in element.rows" :key="item.id">{{item.name}}
+            <button @click="modifyCard(elementIdx, itemIdx)"><font-awesome-icon icon="fa-regular fa-note-sticky" /></button>
+            <button @click="deleteCard(elementIdx, itemIdx)"><font-awesome-icon icon="fa-regular fa-note-sticky" /></button>
+          </div>
         </draggable>
       </div>
       <div class="itemFooter">
-        <div class="btn">
-        + Add another card
+        <div class="btnBox" v-if="element.mode==0" @click="element.mode=1">
+          <div class="btn">
+          + Add another card
+          </div>
+          <div class="btn-img">
+            <font-awesome-icon icon="fa-regular fa-note-sticky" />
+          </div>
         </div>
-        <div class="btn-img">
-          <font-awesome-icon icon="fa-regular fa-note-sticky" />
+        <div class="regBox" style="padding:0px; mn" v-if="element.mode==1">
+          <div>
+            <input v-model="element.addCardTitle" type="text" placeholder="Enter item title for list...">
+          </div>
+          <div>
+            <button class="addBtn" @click="addCard(element)">Add Card</button>
+            <span class="cancleBtn" @click="element.mode=0">✖</span>
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="addBox" @click="addAnotherList()">
-      <div v-if="addMode==0" @click="this.addMode=1">+ Add another list</div>
-      <div v-if="addMode==1"></div>
+    <div class="addBox" @click="addMode=1" v-if="addMode==0" >
+      <div>+ Add another list</div>
+    </div>
+
+    <div class="regBox" v-if="addMode==1">
+      <div>
+        <input v-model="addTitle" type="text" placeholder="Enter list title...">
+      </div>
+      <div>
+        <button @click="addAnotherList()" class="addBtn">Add List</button> 
+        <span @click="addMode=0" class="cancleBtn">✖</span>
+      </div>
     </div>
 
     <div class="addBox">
-      {{data}}
+      {{items}}
+      {{addTitle}}
     </div>
   </div>
 </template>
@@ -46,9 +70,12 @@ export default {
   data() {
     return {
       addMode: 0,
-      data: [
+      addTitle: "",
+      items: [
         {
           id: 1,
+          mode: 0,
+          addCardTitle: "",
           name: 'To-Do-List',
           rows: [
                   {
@@ -63,6 +90,8 @@ export default {
         },
         {
           id: 2,
+          mode: 0,
+          addCardTitle: "",
           name: 'Bucket-List',
           rows: [
                   {
@@ -84,26 +113,47 @@ export default {
   },
   methods: {
     addAnotherList(){
-
+      if(this.addTitle == ""){
+        alert("값을 입력해주세요.");
+        return false;
+      }
+      this.addMode = 0;
       let data = {
-          id: 2,
-          name: 'Bucket-List',
-          rows: [
-                  {
-                    id:3,
-                    name:"맹구"
-                  },
-                  {
-                    id:4,
-                    name:"유리"
-                  },
-                  {
-                    id:5,
-                    name:"훈이"
-                  }
-          ]
+          name: this.addTitle,
+          rows: [],
+          mode: 0,
+          addCardTitle: "",
         }
-      this.data.push(data)
+      this.items.push(data)
+      this.addTitle = ""
+    },
+    addCard(element){
+      if(element.addCardTitle == ""){
+        alert("값을 입력해주세요.")
+      }
+      let data = {
+        id:5,
+        name:element.addCardTitle
+      }
+      element.rows.push(data)
+      element.addCardTitle = ""
+      element.mode = 0;
+    },
+    modifyCard(listIdx, itemIdx){
+      let item = this.items[listIdx]['rows'][itemIdx];
+      item.name = "테스트";
+    },
+    deleteCard(listIdx, itemIdx){
+      this.items[listIdx]['rows'].splice(itemIdx, 1);
+    }
+  },
+  watch: {
+    items: {
+      handler(val, oldVal) {
+        console.log(val)
+        console.log(`watch ${val.name} => ${oldVal.name}`)
+      },
+      deep: true
     }
   }
 }
